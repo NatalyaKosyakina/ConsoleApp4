f014c413760bedfd22625487ada594bd486dc036
natalya_kosyakina <anckamarckowa@yandex.ru>
2023-12-04 07:57:38 +0300

    Домашняя работа

---
+++ Labirint.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Labirint
+    {
+    public static int FindPath(int i, int j, int[,] labirynth1)
+    {
+        Stack<Tuple<int, int>> _path = new Stack<Tuple<int, int>>();
+        Console.WriteLine(labirynth1[i, j]);
+        if (labirynth1[i, j] == 0) _path.Push(new(i, j));
+
+        int count = 0;
+        while (_path.Count > 0)
+        {
+            var current = _path.Pop();
+
+            Console.WriteLine($"{current.Item1},{current.Item2} ");
+            if (labirynth1[current.Item1, current.Item2] == 2)
+            {
+                Console.WriteLine($"Путь найден {current.Item1},{current.Item2} ");
+                count++;
+            }
+
+            labirynth1[current.Item1, current.Item2] = 1;
+
+            if (current.Item1 + 1 < labirynth1.GetLength(0)
+               && labirynth1[current.Item1 + 1, current.Item2] != 1)
+                _path.Push(new(current.Item1 + 1, current.Item2));
+
+            if (current.Item2 + 1 < labirynth1.GetLength(1) &&
+               labirynth1[current.Item1, current.Item2 + 1] != 1)
+                _path.Push(new(current.Item1, current.Item2 + 1));
+
+            if (current.Item1 > 0 && labirynth1[current.Item1 - 1, current.Item2] != 1)
+                _path.Push(new(current.Item1 - 1, current.Item2));
+
+            if (current.Item2 > 0 && labirynth1[current.Item1, current.Item2 - 1] != 1)
+                _path.Push(new(current.Item1, current.Item2 - 1));
+        }
+
+        if (count == 0) { Console.WriteLine("Пути нет"); }
+        return count;
+        }
+
+        public static int FindPath2(int i, int j, int[,] labirynth1)
+        {
+            Stack<Tuple<int, int>> _path = new Stack<Tuple<int, int>>();
+            Console.WriteLine(labirynth1[i, j]);
+            if (labirynth1[i, j] == 0) _path.Push(new(i, j));
+
+            int count = 0;
+            while (_path.Count > 0)
+            {
+                var current = _path.Pop();
+
+                Console.WriteLine($"{current.Item1},{current.Item2} ");
+                if (labirynth1[current.Item1, current.Item2] == 0 
+                    && (current.Item1 == 0 || current.Item1 == labirynth1.GetLength(0) 
+                    || current.Item2 == 0 || current.Item2 == labirynth1.GetLength(1)))
+                {
+                    Console.WriteLine($"Путь найден {current.Item1},{current.Item2} ");
+                    count++;
+                }
+
+                labirynth1[current.Item1, current.Item2] = 1;
+
+                if (current.Item1 + 1 < labirynth1.GetLength(0)
+                   && labirynth1[current.Item1 + 1, current.Item2] != 1)
+                    _path.Push(new(current.Item1 + 1, current.Item2));
+
+                if (current.Item2 + 1 < labirynth1.GetLength(1) &&
+                   labirynth1[current.Item1, current.Item2 + 1] != 1)
+                    _path.Push(new(current.Item1, current.Item2 + 1));
+
+                if (current.Item1 > 0 && labirynth1[current.Item1 - 1, current.Item2] != 1)
+                    _path.Push(new(current.Item1 - 1, current.Item2));
+
+                if (current.Item2 > 0 && labirynth1[current.Item1, current.Item2 - 1] != 1)
+                    _path.Push(new(current.Item1, current.Item2 - 1));
+            }
+
+            if (count == 0) { Console.WriteLine("Пути нет"); }
+            return count;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        public static void Reverse(List<int> ints) {
+            
+            Stack<int> stack = new Stack<int>(ints);
+
+            ints.Clear();
+            ints.AddRange(stack);
+            /*List<int> ints = new List<int> { 1, 2, 3, 4, 5 };
+            Reverse(ints);
+            foreach (int i in ints)
+            {
+                Console.WriteLine(i);
+            }*/
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] labirynth1 = new int[,]
+            {
+                {1, 1, 1, 1, 1, 1, 1 },
+                {1, 1, 0, 0, 0, 0, 1 },
+                {1, 1, 1, 1, 1, 0, 1 },
+                {2, 0, 0, 0, 1, 0, 2 },
+                {1, 1, 0, 2, 1, 1, 1 },
+                {1, 1, 1, 1, 1, 1, 1 },
+                {1, 1, 1, 2, 1, 1, 1 }
+            };
+
+            int[,] labirynth2 = new int[,]
+            {
+            {1, 1, 1, 1, 1, 1, 1 },
+            {1, 0, 0, 0, 0, 0, 1 },
+            {1, 0, 1, 1, 1, 0, 1 },
+            {0, 0, 0, 0, 1, 0, 0 },
+            {1, 1, 0, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 }
+            };
+
+            int count = Labirint.FindPath(3, 1, labirynth1);
+            Console.WriteLine("Количество выходов: " + count);
+
+            count = Labirint.FindPath2(3, 1, labirynth2);
+            Console.WriteLine("Количество выходов: " + count);
+        }
+    }
+}
